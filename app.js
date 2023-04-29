b0d5d104437ba9794d6b724fa7d1a682fe64e92c
Solmi <7548ck@naver.com>
2023-04-29 15:04:17 +0900

    feat: 인사말 기능

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  loginForm.classList.add('hidden');
-  console.log(username);
+  console.log(greeting);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
